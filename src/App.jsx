3248abc8f689aba9a6dbad659ed811348c4d742c
anisa1075 <anisa.nisa1075@gmail.com>
2025-09-15 23:01:58 +0700

    revisi : Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,35 @@ import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Header from './components/headers/Header'
 import Home from './components/home/Home'
-import Qualification from './components/qualification/Qualification'
+// import Qualification from './components/qualification/Qualification'
 import ScrollUp from './components/scrollup/ScrollUp'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
-import Testimonials from './components/testimonials/Testimonials'
+// import Testimonials from './components/testimonials/Testimonials'
 import Work from './components/work/Work'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    
     <div>
-      <Header />
-
-      <main className='main'>
+      <div className='full'>
+        <Header />
         <Home />
+      </div>
+      <main className='main'>
         <About />
         <Skills />
         <Services />
-        <Qualification />
+        {/* <Qualification /> */}
         <Work />
-        <Testimonials />
+        {/* <Testimonials /> */}
         <Contact />
       </main>
       <Footer />
-      <ScrollUp/>
+      <ScrollUp />
     </div>
+
   )
 }
 
